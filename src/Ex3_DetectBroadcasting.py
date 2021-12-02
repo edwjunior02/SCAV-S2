08f164489e14a9ac0442b2af7bde238805842530
Eduard Puig <eduard.puig02@estudiant.upf.edu>
2021-12-02 14:09:17 +0100

    Add ex3

---
+++ src/Ex3_DetectBroadcasting.py
@@ -0,0 +1,30 @@
+
+#BROADCAST STANDARDS RESUME
+# DVB-T ->
+#   Para video tenemos MPEG2 y H.264
+#   Para audio tenemos AAC, Dolby Digital (AC-3) y MP3.
+
+# ATSC ->
+#   Para video tenemos MPEG2 y H.264
+#   Para audio tenemos AC-3 solo.
+
+# DTMB ->
+#   Para video tenemos AVS, AVS+, MPEG2, h.264.
+#   Para audio tenemos DRA, AAC, AC-3, MP2, MP3.
+
+# ISDB-T ->
+#   Para video tenemos MPEG2 (SD channels) y H.264 (HD channels).
+#   Para audio tenemos AAC
+#       ISDB-Tb (Brazilian and Latam) ->
+#           Para video solo H.264
+#           Para audio tenemos AAC.
+
+# Para hacer el ejercicio, simplemente tendremos que analizar los streams de video y detectar en que estandar está.
+# Luego también tendremos que analizar el audio para acabar de detectar el/los estandar/es que se podrian utilizar para
+# retransmitir este archivo.
+
+
+
+
+
+
