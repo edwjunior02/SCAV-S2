4c49a828ca3ff6a1ef00488f15fe19153a61befc
Eduard Puig <eduard.puig02@estudiant.upf.edu>
2021-12-02 01:36:10 +0100

    improving scripts

--- README.md
+++ README.md
@@ -1,4 +1,46 @@
 # SCAV-S2
-Seminar2 repository
+#### Eduard Puig - 194161
 
-Hello world.
+### EJERCICIO 1: ...
+
+
+
+### EJERCICIO 2: MP4 CONTAINER
+Para empezar, tenemos que crear un contenedor ```MP4``` que nos almacene diferentes tracks de audio y video.
+Si tomamos nuestro video ```Resistencia_BM19_cropped.mp4``` como muestra, lo tenemos que añadir a un contenedor que:
+```
+- El video tenga una duración de 1 minuto.
+- Tenga un track/stream MP3 estéreo.
+- Tenga otro track/stream AAC con menor bitrate.
+```
+Primero tenemos que analizar las propiedades y componentes de nuestro video muestra. Para ello hemos hecho un
+diagrama que representa nuestro video:
+
+![](IMAGENES README/ResistenciaMP4-Container.png)
+
+Y queremos convertirlo en un contenedor con estas características:
+
+![](IMAGENES README/NewContainerMP4.png)
+
+Precisamente con ```ffmpeg``` podemos conseguir copiar cierta información de un contenedor a otro y añadir lo que nos falta.
+Primero vamos quedarnos con solo el primer minuto del video ```Resistencia_BM19_cropped.mp4``` por razones de espacio, y vamos a copiar toda la información de este video a un nuevo container mp4 
+a que llamaremos ```Resistencia_MP4_Container.mp4```.
+
+Debido a que ```ffmpeg``` no puede sobreescribir el mismo archivo, hemos realizado el ejercicio en varios pasos:
+```
+1. Extraer audio original del video y convertirlo a .mp3.
+2. Realizar un mapeo de los dos inputs (video muestra y audio extraido en 1) al mismo archivo .mp4
+```
+El código que realiza estas dos funcionalidades es el siguiente:
+```ruby
+    1. os.system("ffmpeg -i Resistencia_MP4_Container.mp4 -c:a mp3 -vn Resistencia_audio.mp3")
+    2. os.system("ffmpeg -i Resistencia_MP4_Container.mp4 -i Resistencia_audio.mp3 -map 0:v -c:v copy -map 1:a -c:a copy -b:a 320k -ac 2 -map 0:a -c:a copy Resistencia_MP4_Container_MP3.mp4")
+```
+
+Como podemos observar, en el primer paso, vemos como extraemos el canal de audio ```-c:a``` y lo extraemos a ```mp3```.
+Seguidamente, importamos los dos archivos (video y audio) y con ```-map 0:v -c:v copy```, estamos copiando el video del primer input ```input #0``` y lo estamos colocando
+en el ```Stream #0:0``` del nuevo archivo.
+A continuación, mapeamos el audio del segundo input ```input #1```, y lo colocamos en el ```Stream #0:1``` del nuevo archivo en mp3 a 320 kbps en estéreo con el bloque ```-map 1:a -c:a copy -b:a 320k -ac 2```.
+Por último, copiamos el codec de audio del archivo original (```input #0```), y lo mapeamos al ```Stream #0:2``` sin modificarlo con el bloque de código ```-map 0:a -c:a copy```.  
+Si ejecutamos el siguiente comando ```ffmpeg -i media/Resistencia_MP4_Container_MP3.mp4``` obtenemos el resultado:
+![](IMAGENES README/Captura de pantalla 2021-12-02 a las 1.31.46.png)
--- src/Ex1_MB&MV.py
+++ src/Ex1_MB&MV.py
@@ -1,6 +1,64 @@
 import os.path
+import time
+import shutil
+
+# Nuestra línea de comando de FFMPEG solo lee imágenes que estén en el mismo directorio que el script en el que se lance la petición.
+#Para ello, haremos una copia de media/Resistencia_BM19_cropped_2.mp4 y la pasaremos al directorio src/.
+pathCarpeta = ("/Users/edwjunior/Documents/UNIVERSIDAD/4o CURSO/1r TRIMESTRE/SISTEMES DE CODIFICACIÓ D'ÀUDIO I VIDEO"
+               "/SEMINARS/SEMINAR 2/media/")
+pathCarpeta2 = ("/Users/edwjunior/Documents/UNIVERSIDAD/4o CURSO/1r TRIMESTRE/SISTEMES DE CODIFICACIÓ D'ÀUDIO I VIDEO"
+                "/SEMINARS/SEMINAR 2/src")
+
+if not os.path.isdir(pathCarpeta):
+    print('la primera carpeta no existe')
+elif not os.path.isdir(pathCarpeta2):
+    print('la segunda carpeta no existe')
+
+contenidos = os.listdir(pathCarpeta)
+for elemento in contenidos:
+    try:
+        if elemento == 'Resistencia_BM19_cropped_2.mp4':
+            print(f"Copiando {elemento} --> {pathCarpeta2} ... ", end="")
+            src = os.path.join(pathCarpeta, elemento)  # origen
+            dst = os.path.join(pathCarpeta2, elemento)  # destino
+            shutil.copy(src, dst)
+            time.sleep(2)
+            print("Correcto")
+        else:
+            continue
+    except:
+        print("Falló")
+        print("Error, no se pudo copiar el archivo. Verifique los permisos de escritura")
+
+print(f"Se han importado los archivos correctamente.")
+time.sleep(2)
 
 
-os.system("ffplay -flags2 +export_mvs -i Resistencia_BM19_cropped.mp4 -vf codecview=mv=pf+bf+bb")
-#os.system("ffmpeg -flags2 +export_mvs -i /Users/edwjunior/Documents/UNIVERSIDAD/4o\ CURSO/1r\ TRIMESTRE/SISTEMES\ DE\ "
-          #"CODIFICACIÓ\ D\'ÀUDIO\ I\ VIDEO/LABS/LAB2-VideoPart/Resistencia_BM19_cropped.mp4 -vf codecview=mv=pf+bf+bb output.mp4")
+# CORE DEL EJERCICIO:---------------------------------------------------------------------------------------------------
+
+os.system("ffmpeg -flags2 +export_mvs -i Resistencia_BM19_cropped_2.mp4 -vf codecview=mvt=fp+bp Resistencia_mvs.mp4")
+print("Reproduciendo video...")
+time.sleep(2)
+os.system("ffplay -flags2 +export_mvs -i /SEMINAR2/media/Resistencia_BM19_cropped_2.mp4 -vf codecview=mv=pf+bf+bb")
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+contenidos = os.listdir(pathCarpeta2)
+for elemento in contenidos:
+    try:
+        if elemento.endswith(".mp4"):
+            print(f"Moviendo {elemento} --> {pathCarpeta} ... ", end="")
+            src = os.path.join(pathCarpeta2, elemento)  # origen
+            dst = os.path.join(pathCarpeta, elemento)  # destino
+            shutil.move(src, dst)       # Ahora utilizamos move en vez de copy, ya que lo queremos mover de aquí.
+            time.sleep(2)
+            print("Correcto")
+        else:
+            continue
+    except:
+        print("Falló")
+        print("Error, no se pudo copiar el archivo. Verifique los permisos de escritura")
+
+time.sleep(2)
+print(f"Se han movido los archivos correctamente.")
+
